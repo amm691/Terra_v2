08d584a59e68bf08db774185b8425be8e87181bc
AmT2 <amitmankar691@gmail.com>
2026-03-04 22:38:09 +0530

    4-3
    4-3

--- gcp-devops/infra/main.tf
+++ gcp-devops/infra/main.tf
@@ -4,7 +4,7 @@ resource "google_container_cluster" "gke" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count = 1
+  initial_node_count       = 1
 
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
@@ -19,10 +19,12 @@ resource "google_container_node_pool" "nodes" {
   cluster  = google_container_cluster.gke.name
   location = var.region
 
-  node_count = 3
+  # CHANGED: Reduced to 2 nodes to fit within CPU quota
+  node_count = 2
 
   node_config {
-    machine_type = "e2-standard-4"
+    # CHANGED: Switched to e2-medium (2 vCPU, 4GB RAM)
+    machine_type = "e2-medium"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
